beb432c5b5a1e6f8e51fbc8a943f56265fe15dd7
Shaarad Dalvi <dalvishaarad@gmail.com>
2019-09-20 22:48:25 +0530

    Added memory indicator

--- dvtm_status.sh
+++ dvtm_status.sh
@@ -5,10 +5,12 @@ FIFO="/tmp/dvtm-status.$$"
 [ -p "$FIFO" ] || mkfifo -m 600 "$FIFO" || exit 1
 
 while true; do
-	DATETIME=$(date | awk '{print $1 " " $2 " " $3 " " $6 " " substr($4,0,5)}')
+	DATETIME=$(date | awk '{print $1 ", " $2 " " $3 "  "substr($4,0,4) " " substr($5,0,5) " " $6}')
 	BATTERY=$(acpi -b | awk '{print $4}' | sed 's/,//g')
-	OUTPUT="$BATTERY $DATETIME"
-	echo $OUTPUT
+	MEMORY=$(free -m | head -2 | tail -1 | awk '{print int($3/$2*10000)/100 }')
+
+	OUTPUT="Memory: $MEMORY% Battery: $BATTERY Date: $DATETIME"
+	echo "$OUTPUT"
 	sleep 60
 done > "$FIFO" &
 
